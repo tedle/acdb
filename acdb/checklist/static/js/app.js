954333d9516aa5ad7cc63c2dc253e4bf79d24032
tedle <tedle@blon.se>
2014-10-10 00:40:16 +0100

    Fish availability checking

--- acdb/checklist/static/js/app.js
+++ acdb/checklist/static/js/app.js
@@ -12,13 +12,13 @@ function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 }]);
 
-acdbApp.controller('ChecklistController', ['$scope', 'acdbApi',
-function($scope, acdbApi) {
+acdbApp.controller('ChecklistController', ['$scope', 'acdbApi', 'date',
+function($scope, acdbApi, date) {
     $scope.test = {
         words: "hey hio hey :)",
     };
 
-    $scope.date = new Date();
+    $scope.date = date;
 
     $scope.sort = {
         order: ['slot'],
@@ -45,13 +45,19 @@ function($scope, acdbApi) {
     });
 }]);
 
-acdbApp.service('acdbApi', ['$http', 'species',
-function($http, species) {
+acdbApp.service('acdbApi', ['$http', 'Species',
+function($http, Species) {
     function get(url) {
         var promise = $http.get(url).
         then(function(response) {
-            response.data.forEach(function(animal) {
-                species.clean(animal);
+            response.data.forEach(function(animal, index) {
+                response.data[index] = new Species(animal['slot'], animal['name'],
+                                                   animal['location'], animal['schedule'],
+                                                   animal['value']);
+                // Doesnt seem worth making 2 inherited classes over such a tiny difference...
+                if ('shadow' in animal) {
+                    response.data[index].shadow = animal['shadow'];
+                }
             });
             return response.data;
         }, function(error) {
@@ -68,57 +74,116 @@ function($http, species) {
     }
 }]);
 
-acdbApp.service('species', ['$filter',
-function ($filter) {
-    this.clean = function(species) {
-        species.caught = false;
-        species.available = this.isAvailable(species.schedule);
-        species.season = this.prettifySeason(species.schedule);
+acdbApp.service('date', [
+function () {
+    var date = new Date();
+    this.get = function() {
+        return date;
+    }
+    this.incMonth = function(num) {
+        date.setMonth(date.getMonth()+num);
+    }
+    this.incDate = function(num) {
+        date.setDate(date.getDate()+num);
+    }
+    this.incHours = function(num) {
+        date.setHours(date.getHours()+num);
+    }
+}]);
+
+acdbApp.factory('Species', ['$filter', 'date',
+function ($filter, date) {
+    function Species(slot, name, habitat, schedule, value) {
+        this.slot = slot;
+        this.name = name;
+        this.habitat = habitat;
+        this.schedule = schedule;
+        this.value = value;
+        this.caught = false;
+        this.season = prettifySeason(this.schedule);
+
     }
 
-    this.isAvailable = function(schedule) {
-        return true;
+    Species.prototype.isAvailable = function() {
+        var seasonal = false;
+        var hourly = false;
+        angular.forEach(this.schedule, function(timeslot) {
+            // Check month range
+            var start = new Date(date.get());
+            start.setMonth(timeslot.month.start - 1);
+            start.setDate(timeslot.day.start);
+            var end = new Date(date.get());
+            end.setMonth(timeslot.month.end - 1);
+            end.setDate(timeslot.day.end);
+
+            if (date.get() >= start && date.get() <= end) {
+                seasonal = true;
+            } else if ((date.get() >= start || date.get() <= end) &&
+                       (timeslot.month.start > timeslot.month.end)) {
+                seasonal = true;
+            }
+            // Check hour range
+            var start = new Date(date.get());
+            start.setHours(timeslot.hour.start);
+            var end = new Date(date.get());
+            end.setHours(timeslot.hour.end);
+
+            if (date.get() >= start && date.get() < end) {
+                hourly = true;
+            } else if ((date.get() >= start || date.get() < end) &&
+                       (timeslot.hour.start > timeslot.hour.end)) {
+                hourly = true;
+            }
+            // Reset for next iteration
+            if (seasonal != hourly) {
+                seasonal = hourly = false;
+            }
+        }, this);
+        return seasonal && hourly;
     }
 
-    this.prettifySeason = function(schedule) {
+    function prettifySeason(schedule) {
         var pretty = "";
         angular.forEach(schedule, function(timeslot, index) {
             if (index > 0) {
                 pretty += ", "
             }
-            date = new Date();
+            season = new Date();
+            // All Year
+            if (timeslot.month.start == 1 && timeslot.month.end == 12) {
+                pretty += "All Year ";
             // July
-            if (timeslot.month.start == timeslot.month.end) {
-                date.setMonth(timeslot.month.start-1);
+            } else if (timeslot.month.start == timeslot.month.end) {
+                season.setMonth(timeslot.month.start-1);
                 // July 15-31
                 if (timeslot.day.start != 1 || timeslot.day.end != 31) {
-                    date.setDate(timeslot.day.start);
-                    pretty += $filter('date')(date,"MMM d-");
-                    date.setDate(timeslot.day.end);
-                    pretty += $filter('date')(date,"d ");
+                    season.setDate(timeslot.day.start);
+                    pretty += $filter('date')(season,"MMM d-");
+                    season.setDate(timeslot.day.end);
+                    pretty += $filter('date')(season,"d ");
                 // July
                 } else {
-                    pretty += $filter('date')(date,"MMM ");
+                    pretty += $filter('date')(season,"MMM ");
                 }
             // July-Sept
             } else {
-                date.setMonth(timeslot.month.start-1);
+                season.setMonth(timeslot.month.start-1);
                 // July 15-Sept
                 if (timeslot.day.start != 1) {
-                    date.setDate(timeslot.day.start);
-                    pretty += $filter('date')(date,"MMM d-");
+                    season.setDate(timeslot.day.start);
+                    pretty += $filter('date')(season,"MMM d-");
                 // July-Sept
                 } else {
-                    pretty += $filter('date')(date,"MMM-");
+                    pretty += $filter('date')(season,"MMM-");
                 }
-                date.setMonth(timeslot.month.end-1);
+                season.setMonth(timeslot.month.end-1);
                 // July-Sept 15
                 if (timeslot.day.end != 31) {
-                    date.setDate(timeslot.day.end);
-                    pretty += $filter('date')(date,"MMM d ");
+                    season.setDate(timeslot.day.end);
+                    pretty += $filter('date')(season,"MMM d ");
                 // July-Sept
                 } else {
-                    pretty += $filter('date')(date,"MMM ");
+                    pretty += $filter('date')(season,"MMM ");
                 }
             }
             // All day
@@ -126,12 +191,14 @@ function ($filter) {
                 pretty += "(All day)";
             // 4pm-9pm
             } else {
-                date.setHours(timeslot.hour.start);
-                pretty += angular.lowercase($filter('date')(date,"(ha-"));
-                date.setHours(timeslot.hour.end);
-                pretty += angular.lowercase($filter('date')(date,"ha)"));
+                season.setHours(timeslot.hour.start);
+                pretty += angular.lowercase($filter('date')(season,"(ha-"));
+                season.setHours(timeslot.hour.end);
+                pretty += angular.lowercase($filter('date')(season,"ha)"));
             }
         });
         return pretty;
     }
+
+    return Species;
 }]);
